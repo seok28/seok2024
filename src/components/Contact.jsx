d075bbe65ca8fd65b12684e20e1fee2594d1160b
seok28 <seokjoonsoo@gmail.com>
2024-07-18 21:21:42 +0900

    contact page add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 const Contact = () => {
-    return <div>Contact</div>
+    return(
+        <section id="contact">
+            <div className="contact__inner">
+                <h2 className="contact__title">Contact</h2>
+                <div className="contact__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="contact__text">
+                    <div className="text">
+                        <div>
+                            <a href="https://www.naver.com/" target="_blank" rel="noreferrer">KAKAO : SEOK28</a>
+                        </div>
+                        <div>
+                            <a href="mailto:seokjoonsoo@naver.com" target="_blank" rel="noreferrer">mail:SEOKJOONSOO@NAVER.COM</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="contact__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
